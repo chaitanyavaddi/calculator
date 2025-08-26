ad0abd8a34c114249f63ed42b76180edd0859d54
Chaitanya Vaddisriram <53746340+chaitanyavaddi@users.noreply.github.com>
2025-08-26 19:43:35 +0530

    fixed bugs

--- hand_cricket.py
+++ hand_cricket.py
@@ -1,23 +1,19 @@
-import streamlit 
-import random
-
+import streamlit, random
 if not streamlit.session_state.get('score'):
     streamlit.session_state.score = 0
-
 streamlit.title('HandCricket Game')
-
 cols = streamlit.columns([1, 1])
-
-
 with cols[1]:
     b = streamlit.button('Play Now', use_container_width=True, type='primary')
     with streamlit.container(border=True):
         streamlit.write('System 🎾')
         c = random.randrange(1, 7)
         streamlit.header(c)
-
 with cols[0]:
     a = streamlit.text_input('Enter Number', label_visibility='collapsed', placeholder='Enter Number Between 1 and 6')
+    if a and (int(a) < 1 or int(a) > 6):
+         streamlit.toast('Enter a number bte 1 and 6')
+         a = 0
     with streamlit.container(border=True):
         streamlit.write('User 🏏')
         if b:
@@ -26,7 +22,8 @@ with cols[0]:
                 streamlit.session_state.score += int(a)
             else:
                  streamlit.toast(f"You are Out, score: {streamlit.session_state.score}")
+        else:
+             streamlit.header(0)
 
 with streamlit.container(border=True):
-        
         streamlit.header(streamlit.session_state.score)
